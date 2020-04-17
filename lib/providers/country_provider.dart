af75145a8a474bde12b72e4c753ad0134a6c681b
awal <awalsetyanto.1993@gmail.com>
2020-04-17 23:01:24 +0700

    add country provider

---
+++ lib/providers/country_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../services/api_services.dart';
+import '../models/country_model.dart';
+
+class CountryProvider with ChangeNotifier {
+  var api = ApiServices();
+  CountryModel country;
+
+  Future<CountryModel> getCountryProvider()async{
+    final response = await api.client.get("${api.baseUrl}/api/country");
+    if (response.statusCode == 200) {
+      notifyListeners();
+      var res = countryModelFromJson(response.body);
+      country = res;
+      return country;
+    }else{
+      return null;
+    }
+  }
+}
