503948795e93133a384b4930f7abb51cd570e793
awal <awalsetyanto.1993@gmail.com>
2020-04-22 21:38:56 +0700

    update homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import './screens/home_page.dart';
 import './providers/homeprovider.dart';
+import './providers/country_provider.dart';
+import './providers/province_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(MultiProvider(providers: [
   ChangeNotifierProvider(create: (_)=> HomeProvider()),
+  ChangeNotifierProvider(create: (_)=> CountryProvider()),
+  ChangeNotifierProvider(create: (_)=> ProvinceProvider()),
 ], child: MyApp()));
 
 class MyApp extends StatelessWidget {
